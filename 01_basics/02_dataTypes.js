4b016e573b9d8faa90f1ec7960f89258b503c60e
Vermamanish20 <manishkumar636p@gmail.com>
2024-09-03 17:53:00 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+//code readability should be high
+
+console.log("manish")
+
+let name = "manish"
+let age = 18
+let isLoggedIn = false
+
+// numebr => 2 to power 53 
+// bigint
+// string => ""
+// boolean  => true/false
+// null => standlone value 
+// undefined =>
+// symbol => unique 
+
+
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
