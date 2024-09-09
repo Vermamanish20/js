0d10c196f9f4bb9abfd9abeb5d4eaaf087099e94
Vermamanish20 <manishkumar636p@gmail.com>
2024-09-09 08:39:25 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,17 @@
+// var c = 300
+let c = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("Inner:", a);
+}
+
+for (let i = 0; index < array.length; index++) {
+    const element = array[i];
+    
+}
+
+console.log(c);
+// console.log(a);
+// console.log(b);
