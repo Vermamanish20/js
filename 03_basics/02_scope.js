e2ee9c77bcffd5fdc895a120cc2fb152703f3701
Vermamanish20 <manishkumar636p@gmail.com>
2024-09-09 08:59:17 +0000

    scopes

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,17 +1,57 @@
 // var c = 300
 let c = 300
-
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner:", a);
+    // console.log("Inner:", a);
+
 }
 
-for (let i = 0; index < array.length; index++) {
-    const element = array[i];
-    
-}
+
+
 
 console.log(c);
 // console.log(a);
 // console.log(b);
+
+function one() {
+    const username = "manish"
+
+    function two() {
+        const website = "Github"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "manish"
+    if (username === "manish") {
+        const website = "Github"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username)
+
+// +++++++++++++ interesting ++++++++++
+
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+} 
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
